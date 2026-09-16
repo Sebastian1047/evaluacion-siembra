647e110ec9b5df336194eb93c5862257a6b62d44
Sebastian1047 <94562946+Sebastian1047@users.noreply.github.com>
2026-09-16 17:23:14 -0500

    Cargar sincronización Azure de participantes

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -24,7 +24,8 @@
     'js/week-lifecycle.js?v=20260912-1',
     'js/calendar-week-control.js?v=20260916-1',
     'js/analyst-conformity-calculations.js?v=20260916-1',
-    'js/test-mode-week-control.js?v=20260916-1'
+    'js/test-mode-week-control.js?v=20260916-1',
+    'js/azure-participant-sync.js?v=20260916-1'
   ];
 
   for (const src of scripts) {
