f29f7a4898c951ef66d3b9eac939fe36cd219828
Sebastian1047 <94562946+Sebastian1047@users.noreply.github.com>
2026-09-16 21:48:23 -0500

    Cargar normalización visual al final

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -26,7 +26,8 @@
     'js/analyst-conformity-calculations.js?v=20260916-1',
     'js/test-mode-week-control.js?v=20260916-3',
     'js/azure-participant-sync.js?v=20260916-1',
-    'js/azure-evaluation-sync.js?v=20260916-1'
+    'js/azure-evaluation-sync.js?v=20260916-1',
+    'js/ui-stability.js?v=20260916-1'
   ];
 
   for (const src of scripts) {
