0406dceecfe043a58aed2e0c9e570b62775e1d01
Sebastian1047 <94562946+Sebastian1047@users.noreply.github.com>
2026-09-18 12:06:14 -0500

    Load Azure week state reconstruction fix

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -26,7 +26,7 @@
     'js/week-lifecycle.js?v=20260912-1',
     'js/calendar-week-control.js?v=20260916-1',
     'js/analyst-conformity-calculations.js?v=20260916-1',
-    'js/test-mode-week-control.js?v=20260917-4',
+    'js/test-mode-week-control.js?v=20260918-5',
     'js/azure-participant-sync.js?v=20260917-2',
     'js/azure-participant-state.js?v=20260917-1',
     'js/azure-evaluation-sync.js?v=20260916-1'
