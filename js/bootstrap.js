2e34e9c5a0fd5d047001ffcaf13c3689309b5915
Sebastian1047 <94562946+Sebastian1047@users.noreply.github.com>
2026-09-23 10:33:48 -0500

    Load offline readiness indicator

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -9,7 +9,8 @@
     // terminan de cargar las integraciones posteriores.
     'js/monitor-current-ui.js?v=20260916-2',
     'js/offline-storage.js?v=20260923-1',
-    'js/offline-bootstrap.js?v=20260923-1',
+    'js/offline-bootstrap.js?v=20260923-2',
+    'js/offline-readiness.js?v=20260923-1',
     'js/week-options.js',
     'js/empleados-reales.js',
     'js/historial-analista.js?v=20260923-real-azure-history',
